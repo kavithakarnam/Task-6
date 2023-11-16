95c76f118c39296cd15c7619bc8c43cad46cb6a7
kavithakarnam <150236859+kavithakarnam@users.noreply.github.com>
2023-11-16 19:38:10 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,88 @@
+// Arithmetic Operators//
+let num1=8;
+let num2=3;
+
+//Addition
+console.log(num1+num2 );
+
+//Subtraction
+console.log(num1-num2 );
+
+//Multiplication
+console.log(num1*num2);
+
+//Division
+console.log(num1/num2);
+
+//Modulus
+console.log(num1%num2);
+
+//logical operator//
+
+let x=true;
+let y=false;
+
+//logical AND
+
+console.log(x&&y);
+
+//logical OR
+console.log(x||y);
+
+//logical NOT
+console.log(!x);
+
+//Comparision operator//
+
+let a=50;
+let b=30;
+
+//equal to
+console.log(a==b);
+
+//Strict equal to
+console.log(a===b);
+
+//Not equal to
+console.log(a!=b);
+
+//Strict not equal to
+console.log(a!==b);
+
+//Less than
+console.log(a<b);
+
+//Grater than
+console.log(a>b);
+
+//Less than or Equal to
+console.log(a<=b);
+
+//Greater than or Equal to
+console.log(a>=b);
+
+
+//Assignment Operator//
+
+let  num3=200;
+
+//Addition Assignment
+num3+=20;
+console.log(num3);
+
+//Subtraction Assignment
+num3-=40;
+console.log(num3-num2);
+
+//Multiplication Assignment
+num3*=25;
+console.log(num3);
+
+//Division Assignment
+num3/=15;
+console.log(num3);
+
+//modulus Assignment
+num3%=10;
+console.log(num3);
+
